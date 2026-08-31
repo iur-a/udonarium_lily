628cc26f195825361871bece437a5a6fc9bf2eda
iur-a <story2episode1@gmail.com>
2026-08-31 23:04:32 +0900

    feat: チャットHTMLタグ装飾対応・チュートリアル追加

--- src/app/component/chat-message/chat-message.component.ts
+++ src/app/component/chat-message/chat-message.component.ts
@@ -82,6 +82,13 @@ export class ChatMessageComponent implements OnInit, AfterViewInit {
     // 記入例：|永遠力暴風雪《エターナルフォースブリザード》
     // 振られる側に《スキル名》は有効：|《約束された勝利の剣》《エクスカリバー》
     let escapeText = this.escapeHtml(text);
+    // <br> 改行
+    escapeText = escapeText.replace(/&lt;\/?br\s*\/?&gt;/gi, '<br>');
+    // 属性なし装飾タグ: b strong i em u s strike small sup sub mark
+    escapeText = escapeText.replace(/&lt;(\/?(?:b|strong|i|em|u|s|strike|small|sup|sub|mark))&gt;/gi, '<$1>');
+    // <font color="..."> color値は英数字と#のみ許容
+    escapeText = escapeText.replace(/&lt;font\s+color=&quot;([a-zA-Z0-9#]+)&quot;&gt;/gi, '<font color="$1">');
+    escapeText = escapeText.replace(/&lt;\/font&gt;/gi, '</font>');
     return escapeText.replace(/[\|｜]([^\|｜\s]+?)《(.+?)》/g, '<ruby style="white-space:normal;">$1<rt>$2</rt></ruby>').replace(/\\s/g,' ');
   }
 
--- src/app/component/chat-tab/chat-tab.component.ts
+++ src/app/component/chat-tab/chat-tab.component.ts
@@ -46,6 +46,7 @@ export class ChatTabComponent implements OnInit, AfterViewInit, OnDestroy, OnCha
     { from: 'System', to: '???', timestamp: 0, imageIdentifier: '', tag: '', name: 'チュートリアル > プレイヤー', text: 'また、過去のダイレクトメッセージはあなたのIDが更新されると同じルーム内であっても見えなくなります。注意してください。' },
     { from: 'System', timestamp: 0, imageIdentifier: '', tag: '', name: 'チュートリアル', text: '動作推奨環境はデスクトップChromeです。今のところ、スマホからだと上手く操作できません。' },
     { from: 'System', timestamp: 0, imageIdentifier: '', tag: '', name: 'チュートリアル', text: 'リリィ追加機能：チャット簡易表示機能は最初のチャット入力を入力すると使用可能になります。' },
+    { from: 'System', timestamp: 0, imageIdentifier: '', tag: '', name: 'チュートリアル', text: 'リリィ追加機能：チャットでHTMLタグによる装飾が使えます。<br><b>太字</b>：&lt;b&gt; / <i>斜体</i>：&lt;i&gt; / <u>下線</u>：&lt;u&gt; / <s>取消線</s>：&lt;s&gt; / <small>小文字</small>：&lt;small&gt; / <sup>上付</sup>：&lt;sup&gt; / <sub>下付</sub>：&lt;sub&gt; / <mark>ハイライト</mark>：&lt;mark&gt; / <font color="red">文字色</font>：&lt;font color="red"&gt; / 改行：&lt;br&gt;' },
     { from: 'System', timestamp: 0, imageIdentifier: '', tag: '', name: 'チュートリアル', text: 'チュートリアルは以上です。このチュートリアルは最初のチャットを入力すると非表示になります。' },
   ];
 
